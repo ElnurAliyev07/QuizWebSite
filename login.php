fe4e41153415935dcc13029689d08f37132b717b
ElnurAliyev07 <115114253+ElnurAliyev07@users.noreply.github.com>
2023-10-30 17:18:09 +0400

    Update login.php

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ if (isset($_SESSION["user"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
