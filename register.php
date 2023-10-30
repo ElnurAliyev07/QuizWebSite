9c784b1909ba2743c9db24a4f5f9c99a06802335
ElnurAliyev07 <115114253+ElnurAliyev07@users.noreply.github.com>
2023-10-30 17:18:22 +0400

    Update register.php

--- register.php
+++ register.php
@@ -12,7 +12,7 @@ if (isset($_SESSION["user"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Form</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
